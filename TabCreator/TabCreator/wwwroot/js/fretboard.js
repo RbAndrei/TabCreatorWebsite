e61a64db5e6f0ebf00848af8b1de59f11a633724
AndreiRb <andrei.roibu@yahoo.com>
2024-04-17 01:12:15 +0300

    WORKING FRETBOARD VERSION

--- TabCreator/TabCreator/wwwroot/js/fretboard.js
+++ TabCreator/TabCreator/wwwroot/js/fretboard.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     const root = document.documentElement;
     const fretboard = document.querySelector('.fretboard');
-    const accidentalSelector = document.querySelector('.accidental-selector');
+    const chordSection = document.querySelector('.chord');
     const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
     const doubleFretMarkPositions = [12, 24];
     const notesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
@@ -10,7 +10,8 @@
     const guitarStandardTuning = [4, 11, 7, 2, 9, 4];
 
     let allNotes;
-    let accidentals = 'flats';
+    let accidentals = 'sharps';
+    let activeNotes = ['E4', 'B3', 'G3', 'D3', 'A2', 'E2'];
     let numberOfFrets = 25;
     let numberOfStrings = 6;
     let currentOctave = 2;
@@ -18,11 +19,11 @@
     const app = {
         init() {
             this.setupFretboard();
+            this.setupChordSection();
             handlers.setupEventListeners();
         },
         setupFretboard() {
             fretboard.innerHTML = '';
-            root.style.setProperty('--number-of-strings', numberOfStrings);
             // Add strings to fretboard
             for (let i = 0; i < numberOfStrings; i++) {
                 currentOctave = openNotesOctave[i];
@@ -35,6 +36,16 @@
                 for (let fret = 0; fret <= numberOfFrets; fret++) {
                     let noteFret = tools.createElement('div');
                     noteFret.classList.add('note-fret');
+
+                    let noteBtn = tools.createElement('button');
+                    noteBtn.classList.add('note-btn');
+                    noteBtn.classList.add('string-' + (i + 1));
+                    noteBtn.classList.add('fret-' + fret);
+                    noteBtn.addEventListener('click', function (event) {
+                        handlers.setNoteSelected(event);
+                    });
+
+                    noteFret.appendChild(noteBtn);
                     string.appendChild(noteFret);
 
                     let noteName = this.generateNoteNames((fret + guitarStandardTuning[i]), accidentals, currentOctave);
@@ -43,7 +54,7 @@
                         currentOctave += 1;
                     }
 
-                    noteFret.setAttribute('data-note', noteName);
+                    noteBtn.innerHTML = noteName;
 
                     // Add single fret marks
                     if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
@@ -58,7 +69,18 @@
 
                 }
             }
-            allNotes = document.querySelectorAll('.note-fret');
+            allNotes = document.querySelectorAll('.note-btn');
+        },
+        setupChordSection() {
+            chordSection.innerHTML = '';
+            let noteNames = activeNotes
+
+            for (let i = 0; i < numberOfStrings; i++) {
+                let noteNameElement = tools.createElement('p', noteNames[i]);
+                noteNameElement.classList.add("string-" + (i + 1));
+                noteNameElement.classList.add("pressed-note");
+                chordSection.appendChild(noteNameElement);
+            }
         },
         generateNoteNames(noteIndex, accidentals, octave) {
             noteIndex = noteIndex % 12;
@@ -71,20 +93,51 @@
             noteName = noteName + octave.toString();
 
             return noteName;
+        },
+        updateChord() {
+            const pressedNotes = document.querySelectorAll('.pressed-note');
+
+            for (let i = 0; i < numberOfStrings; i++) {
+                pressedNotes[i].innerHTML = activeNotes[i];
+            }
         }
     }
 
     const handlers = {
-        setAccidentals(event) {
-            if (event.target.classList.contains('acc-select')) {
-                accidentals = event.target.value;
-                app.setupFretboard();
-            } else {
-                return;
-            }
-        },
         setupEventListeners() {
-            accidentalSelector.addEventListener('click', this.setAccidentals);
+
+        },
+        setNoteSelected(event) {
+            let currentElement = event.target;
+            let currentString = currentElement.classList.value.split(" ")[1];
+            let currentFret = currentElement.classList.value.split(" ")[2];
+            let stringNumber = currentString.slice(-1) - 1;
+
+            if (currentElement.classList.contains('note-selected')) {
+                currentElement.classList.remove('note-selected');
+                currentElement.style.background = '#2f4d85';
+
+                activeNotes[stringNumber] = 'X';
+            }
+            else {
+                currentElement.classList.add('note-selected');
+                currentElement.style.background = 'black';
+
+                //Deselect all other selected notes on same string
+
+                /*let currentString = currentElement.classList.value.split(" ")[1];
+                let currentFret = currentElement.classList.value.split(" ")[2];*/
+
+                for (const element of allNotes) {
+                    if (element.classList.contains(currentString) && !element.classList.contains(currentFret)) {
+                        element.classList.remove('note-selected');
+                        element.style.background = '#2f4d85';
+                    }
+                }
+
+                activeNotes[stringNumber] = currentElement.innerHTML;
+            }
+            app.updateChord();
         }
     }
 
