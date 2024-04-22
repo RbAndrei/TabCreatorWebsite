ccd65b5749e8b97c6f6609a1a1f07a7cfaa993d4
AndreiRb <andrei.roibu@yahoo.com>
2024-04-22 19:16:48 +0300

    String with note IDs created and correctly updated.

--- TabCreator/TabCreator/wwwroot/js/tab.js
+++ TabCreator/TabCreator/wwwroot/js/tab.js
@@ -1,14 +1,27 @@
-﻿(function () {
+﻿/* ********************************************************
+    Things to remember:
+
+    Each tab has an id of form: tab-x , where x is the number of the tabBlock
+    Each string has an id of form: tab-x-y , where x is the number of the tabBlock the string is a child of
+    and y is the number of the string in that tab block
+    Each note container has an id of form: note-x-y-z , where x is the number of the tabBlock the string is a child of,
+    y is the number of the string that the note is a part of, z is the number of the note on the string.
+
+    The tab composition string will contain the ids of the notes that are currently used and:
+        - if the noteContainer contains a fret number, the number of the fret. NOTE: the fret number will always
+                                                                               be made up of 2 numbers (e.g 00, 07, 12, 35)
+        - if the noteContainer contains an articultation: - sl => slide
+                                                          - po => pull-off
+                                                          - ho => hammer-on
+******************************************************** */
+
+(function () {
     const root = document.documentElement;
     const scoreElement = document.querySelector('.score-element');
     const numberOfStrings = 6;
     const numberOfNotes = 36;
-    const noteColors = {
-        'default': '#2f4d85',
-        'defaultmouse': '#4370c4',
-    }
 
-    let allNotes;
+    let tabCompositionString = '';
     let selectedElement = document.querySelector('#fret-number');
 
     const app = {
@@ -20,22 +33,25 @@
         },
         setupTabBlock() {
 
-            for (let stuff = 0; stuff < 15; stuff++) {
+            for (let blockNumber = 0; blockNumber < 15; blockNumber++) {
                 let tabBlock = tools.createElement('div');
+                tabBlock.id = "tab-" + blockNumber;
                 tabBlock.classList.add('tab-block');
 
                 scoreElement.appendChild(tabBlock);
 
                 tabBlock.innerHTML = '';
                 // Add strings to tabBlock
-                for (let i = 0; i < numberOfStrings; i++) {
+                for (let stringNumber = 0; stringNumber < numberOfStrings; stringNumber++) {
                     let string = tools.createElement('div');
+                    string.id = "string-" + blockNumber + "-" + stringNumber;
                     string.classList.add('string');
                     tabBlock.appendChild(string);
 
                     // Create note containers
                     for (let noteNumber = 0; noteNumber <= numberOfNotes; noteNumber++) {
                         let noteContainer = tools.createElement('div');
+                        noteContainer.id = "note-" + blockNumber + "-" + stringNumber + "-" + noteNumber;
                         noteContainer.classList.add('note-container');
 
                         string.appendChild(noteContainer);
@@ -44,10 +60,6 @@
                         noteText.classList.add('note-text');
 
                         noteContainer.appendChild(noteText);
-
-                        if (noteNumber % 2 == 1) {
-                            noteContainer.classList.add('articulation');
-                        }
 
                         noteContainer.addEventListener('mouseover', function (event) { handlers.setNoteMouseOver(event); })
                         noteContainer.addEventListener('mouseout', function (event) { handlers.setNoteMouseOut(event); })
@@ -106,6 +118,20 @@
 
             let fretNumber = 0;
 
+            let currentId = parentElement.id;
+            let currentBlockId = parseInt(currentId.split("-")[1]);
+            if (currentBlockId < 10) {
+                currentBlockId = '0' + currentBlockId;
+            }
+            let currentStringId = currentId.split("-")[2];
+
+            let currentNoteId = parseInt(currentId.split("-")[3]);
+            if (currentNoteId < 10) {
+                currentNoteId = '0' + currentNoteId;
+            }
+
+            currentId = currentBlockId + currentStringId + currentNoteId;
+
             if (selectedElement.value === undefined) {
                 fretNumber = selectedElement.innerHTML;
             }
@@ -121,12 +147,55 @@
 
             if (parentElement.classList.contains('note-selected')) {
                 parentElement.classList.remove('note-selected');
+
+                switch (childElement.innerHTML) {
+                    case 'p':
+                        currentId += 'po';
+                        break;
+                    case 'h':
+                        currentId += 'ho';
+                        break;
+                    case '/':
+                        currentId += 'sl';
+                        break;
+                    default:
+                        if (childElement.innerHTML < 10) {
+                            currentId += '0' + childElement.innerHTML;
+                        }
+                        else currentId += childElement.innerHTML;
+                        break;
+                }
+
                 childElement.innerHTML = '';
+                
+                tabCompositionString = tabCompositionString.replace(currentId, '');
             }
             else {
                 parentElement.classList.add('note-selected');
                 childElement.innerHTML = fretNumber;
+
+                switch (childElement.innerHTML) {
+                    case 'p':
+                        currentId += 'po';
+                        break;
+                    case 'h':
+                        currentId += 'ho';
+                        break;
+                    case '/':
+                        currentId += 'sl';
+                        break;
+                    default:
+                        if (childElement.innerHTML < 10) {
+                            currentId += '0' + childElement.innerHTML;
+                        }
+                        else currentId += childElement.innerHTML;
+                        break;
+                }
+
+                tabCompositionString += currentId;
             }   
+
+            console.log(tabCompositionString);
         },
         setNoteMouseOver(event) {
             let [parentElement, childElement] = tools.getParentChild(event.target, 'note-container', 'note-text');
@@ -138,7 +207,6 @@
         setNoteMouseOut(event) {
             let [parentElement, childElement] = tools.getParentChild(event.target, 'note-container', 'note-text');
 
-            parentElement.style.color = noteColors['default'];
             parentElement.classList.remove('container-hover');
             childElement.classList.remove('container-hover');
         }
